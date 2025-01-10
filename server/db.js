217d47afdc9a0e7066666973f5e2c32de2b8bc5a
jessewiegand21 <jessewiegand21@yahoo.com>
2025-01-09 21:16:27 -0700

    createTables

--- server/db.js
+++ server/db.js
@@ -1,4 +1,49 @@
-const pg = require('pg')
-const express = require('express')
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/the_acme_notes_db')
-const app = express()
+const pg = require("pg");
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/drPepper_db"
+);
+
+const uuid = require("uuid");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWT = process.env.JWT || "shhh";
+
+const createTables = (async) => {
+  const SQL = `  
+
+    
+    CREATE TABLE users(
+        id UUID PRIMARY KEY,
+        username VARCHAR(20) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT now(),
+        photo_URL VARCHAR(255)
+        );
+    CREATE TABLE flavors(
+        id UUID PRIMARY KEY,
+        name VARCHAR(50) UNIQUE NOT NULL,
+        created_at TIMESTAME DEFAULD now(),
+        description VARCHAR(255),
+        photo_URL VARCHAR(255),
+        average_Score REAL,
+        disabled BOOLEAN DEFAULT false
+        );
+    CREATE TABLE reviews(
+        id UUID PRIMARY KEY,
+        user_id UUID REFERENCES users(id) NOT NULL,
+        flavor_id UUID REFERENCES flavors(id) NOT NULL,
+        content VARCHAR(500),
+        score INTEGER
+        );
+    CREATE TABLE comments(
+        id UUID PRIMARY KEY,
+        user_id UUID REFERENCES users(id) NOT NULL,
+        flavor_id UUID REFERENCES flavors(id) NOT NULL,
+        content VARCHAR(500) NOT NULL,
+        );
+    `;
+};
+module.exports = {
+  client,
+  createTables,
+};
---
+++ server/index.js
@@ -0,0 +1,30 @@
+const { client, createTables } = require("./db");
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+// app routes go here............
+
+app.get("/", (res, req, next) => {
+  try {
+  } catch (error) {}
+});
+
+const init = async () => {
+  const port = process.env.PORT || 3000;
+
+  await client.connect();
+
+  console.log("connected to database");
+
+  await createTables();
+
+  console.log("tables created");
+
+  app.listen(port, () => console.log(`listening on port ${port}`));
+};
+
+init();

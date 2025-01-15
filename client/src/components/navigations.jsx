bced625a8957675776c295dda76de01e2ce6f3c8
Jared Schwartz <jareds1070@gmail.com>
2025-01-14 20:26:51 -0600

    Finished the single item and reviews display

--- client/src/components/navigations.jsx
+++ client/src/components/navigations.jsx
@@ -2,8 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function Navigation({ user }) {
   return (
-    <nav className="navbar">
+    <nav id="navbar">
+      <img src="https://www.pngall.com/wp-content/uploads/15/Dr-Pepper-Logo-PNG-Image-HD.png" width="50px"></img>
+      <h1>Dr Pepper Reviews</h1>
+      <div id="links">
       <Link to="/">Home</Link>
+      <Link to="/flavors">Flavors</Link>
+      <Link to="/reviews">Reviews</Link>
+      <Link to="/login">Login</Link>
+      </div>
     </nav>
   );
 }
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-function Home({user, setUser, token, setToken}){
+export default function Home({user, setUser, token, setToken}){
+    const navigate = useNavigate();
 
     return (
         <>
+        <div id="home">
+        <h1>Which is the highest rated Dr Pepper flavor?</h1>
+        <img src="https://s.wsj.net/public/resources/images/BN-MP782_0217dr_G_20160216103055.jpg"></img>
+        <h3>Highest Rated Flavor: (Answer Here)</h3>
+        </div>
         </>
     )
 }
 
-export default Home;

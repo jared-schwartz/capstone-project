7b2be95b4e241676a522b4fe58382d88082f0a76
Johnathan Pedersen <jpedersen2005@gmail.com>
2025-01-23 20:04:30 -0800

    bug fixes

--- server/db.js
+++ server/db.js
@@ -1,5 +1,4 @@
-const pg = require("pg");
-const { client } = require("server.js");
+const { client } = require("./server.js");
 const uuid = require("uuid");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -13,7 +12,7 @@ const createTables = async () => {
   DROP TABLE IF EXISTS flavors;
 
     CREATE TABLE users(
-        id UUID PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         username VARCHAR(20) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
         created_at TIMESTAMP DEFAULT now(),
@@ -21,7 +20,7 @@ const createTables = async () => {
         );
 
     CREATE TABLE flavors(
-        id UUID PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         name VARCHAR(50) UNIQUE NOT NULL,
         created_at TIMESTAMP DEFAULT now(),
         description VARCHAR(255),
@@ -30,29 +29,41 @@ const createTables = async () => {
         disabled BOOLEAN DEFAULT false
         );
     CREATE TABLE reviews(
-        id UUID PRIMARY KEY,
-        user_id UUID REFERENCES users(id) NOT NULL,
-        flavor_id UUID REFERENCES flavors(id) NOT NULL,
+        id SERIAL PRIMARY KEY,
+        user_id SERIAL REFERENCES users(id) NOT NULL,
+        flavor_id SERIAL REFERENCES flavors(id) NOT NULL,
         content VARCHAR(500),
-        score INTEGER,
-        username VARCHAR(20) UNIQUE NOT NULL;
+        score INTEGER
         );
     CREATE TABLE comments(
-        id UUID PRIMARY KEY,
-        user_id UUID REFERENCES users(id) NOT NULL,
-        review_id UUID REFERENCES reviews(id) NOT NULL,
+        id SERIAL PRIMARY KEY,
+        user_id SERIAL REFERENCES users(id) NOT NULL,
+        review_id SERIAL REFERENCES reviews(id) NOT NULL,
         content VARCHAR(500) NOT NULL
         );
     `;
   await client.query(SQL);
 };
 
+const seedData = async () => {
+  createUser({username:"johnathan", password:"j", photo_URL:""});
+  createFlavor({ name: "Dr Pepper Original", description: "good", photo_URL: "https://i5.walmartimages.com/seo/Doctor-Pepper-Soda-12oz-Cans-Pack-of-48_f311a391-8c1c-4b38-b7f7-788d26359f44.4eb13d8fade9191d2c4f05fdc7cca03f.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF" }),
+  createFlavor({ name: "Dr Pepper Vanilla Float", description: "good", photo_URL: "https://i5.walmartimages.com/asr/1e49efb6-01c2-415f-9965-29e15fc3432d.cc510533258805a04cb12e8d5501ca94.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF" }),
+  createFlavor({ name: "Dr Pepper Cherry", description: "good", photo_URL: "https://images.albertsons-media.com/is/image/ABS/960022623-R001?$ng-ecom-pdp-mobile$&defaultImage=Not_Available" }),
+  createFlavor({ name: "Diet Dr Pepper",description: "good", photo_URL: "https://i5.walmartimages.com/asr/097a28e0-cb1d-4ce9-916c-16b4b8d395a9.1e4e4705ac25584781fadea7ee4768d1.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF" }),
+  createFlavor({ name: "Dr Pepper Strawberries & Cream",description: "good", photo_URL: "https://i5.walmartimages.com/asr/028be2fd-151e-4a7a-9438-c6d140e76001.4a9f1df900d85b116abb7b8cde88ed47.jpeg" }),
+  createFlavor({ name: "Dr Pepper Creamy Coconut",description: "good", photo_URL: "https://www.kroger.com/product/images/large/back/0007800003871" }),
+  createFlavor({ name: "Dr Pepper Dark Berry",description: "good", photo_URL: "https://i5.walmartimages.com/asr/e663d659-1a5f-422e-94b7-1f5abd3c60fb.0358507f00c74fb01e767a15ea6fa417.jpeg" }),
+  createFlavor({ name: "Dr Pepper Cherry Vanilla",description: "good", photo_URL: "https://sweetkingdoms.com/wp-content/uploads/2023/11/DR-PEPPER-CHERRY-VANILLA.webp" }),
+  createFlavor({ name: "Dr Pepper Cream Soda",description: "good", photo_URL: "https://i.ebayimg.com/images/g/3k4AAOSwSAFhKJAQ/s-l1200.jpg" }),
+  createFlavor({ name: "Dr Pepper Original (Real Sugar)",description: "good", photo_URL: "https://m.media-amazon.com/images/I/61kw9c37qRL.jpg" })
+};
+
 const createUser = async ({ username, password, photo_URL }) => {
   const SQL = `
-      INSERT INTO users(id, username, password, photo_URL) VALUES($1, $2, $3, $4) RETURNING *
+      INSERT INTO users(username, password, photo_URL) VALUES($1, $2, $3) RETURNING *
     `;
   const response = await client.query(SQL, [
-    uuid.v4(),
     username,
     await bcrypt.hash(password, 5),
     photo_URL,
@@ -62,25 +73,22 @@ const createUser = async ({ username, password, photo_URL }) => {
 
 const createFlavor = async ({ name, description, photo_URL }) => {
   const SQL = `
-        INSERT INTO flavors(id, name, description, photo_URL) VALUES($1, $2, $3, $4) RETURNING *
-      `;
-  const response = await client.query(SQL, [
-    uuid.v4(),
-    name,
-    description,
-    photo_URL,
-  ]);
+    INSERT INTO flavors (name, description, photo_URL) 
+    VALUES ($1, $2, $3) 
+    RETURNING *;
+  `;
+  const response = await client.query(SQL, [name, description, photo_URL]);
   return response.rows[0];
 };
 
-const createReview = async ({ user_id, flavor_id, content, score }) => {
+const createReview = async ({ id, user_id, flavor_id, content, score }) => {
   const SQL = `
     INSERT INTO reviews (id, user_id, flavor_id, content, score) 
     VALUES ($1, $2, $3, $4, $5) 
     RETURNING *`;
 
   const response = await client.query(SQL, [
-    uuid.v4(), 
+    id,
     user_id, 
     flavor_id, 
     content, 
@@ -117,4 +125,5 @@ module.exports = {
   createFlavor,
   fetchFlavors,
   createReview, 
+  seedData
 };
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const {
   createFlavor,
   fetchFlavors,
   createReview,
+  seedData
 } = require("./db");
 
 const express = require("express");
@@ -33,7 +34,7 @@ app.get("/api/flavors", async (res, req, next) => {
   }
 });
 
-app.post("/api/reviews", async (req, res, next) => {
+app.post("/api/review", async (req, res, next) => {
   try {
     const { user_id, flavor_id, content, score } = req.body;
 
@@ -47,15 +48,6 @@ app.post("/api/reviews", async (req, res, next) => {
   }
 });
 
-app.post("/api/register", async (req, res, next) => {
-  try {
-    const {} = req.body;
-    createUser({username: alt, password: pw, photo_URL: ""});
-  }
-  catch (ex) {
-    next(ex);
-  }
-});
 
 const init = async () => {
   const port = process.env.PORT || 3000;
@@ -66,29 +58,7 @@ const init = async () => {
 
   await createTables();
 
-  console.log("tables created");
-
-  const [jesse, jared, karl, joanathan, original, vanilla, cherry, diet] = await Promise.all([
-    createUser({ username: "jesse", password: "j", photo_URL: "" }),
-    createUser({ username: "jared", password: "j", photo_URL: "" }),
-    createUser({ username: "karl", password: "k", photo_URL: "" }),
-    createUser({ username: "joanathan", password: "j", photo_URL: "" }),
-    createFlavor({ name: "original", description: "good", photo_URL: "" }),
-    createFlavor({ name: "vanilla", description: "good", photo_URL: "" }),
-    createFlavor({ name: "cherry", description: "good", photo_URL: "" }),
-    createFlavor({ name: "diet", description: "good", photo_URL: "" }),
-  ]);
-
-  console.log("Users and flavors created");
-
-  const [review1, review2, review3, review4] = await Promise.all([
-    createReview({ user_id: karl.id, flavor_id: cherry.id, content: "It tastes like my grandma's ashes", score: 1 }),
-    createReview({ user_id: jared.id, flavor_id: original.id, content: "Nothing beats the original", score: 5 }),
-    createReview({ user_id: joanathan.id, flavor_id: vanilla.id, content: "Reminds me of my childhood", score: 4 }),
-    createReview({ user_id: jesse.id, flavor_id: diet.id, content: "", score: 3 })
-  ]);
-
-  console.log("Reviews created");
+  await seedData();
 
   app.listen(port, () => console.log(`listening on port ${port}`));
 };

73a932346008214af3a4a37c263b4adc5916d2aa
KarlB2 <karlbusse242@gmail.com>
2025-01-30 22:24:07 -0600

    merging into userpage

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const pg = require("pg");
 const client = new pg.Client({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "root",
+    host: "localhost",
+    port: 5432,
+    user: "karl",
+    password: "password",
+    database: "drPepper_db"
 });
 module.exports = {
-  client,
+    client,
 };
